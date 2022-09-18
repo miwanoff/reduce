f8688c1384d1ce1dc1465e681d76bf89b74e53c6
miwanoff <marina.iva.noffa@gmail.com>
2022-09-18 12:45:46 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let m = 15;
+let n = 5;
+if (n === 0) {
+  /* todo*/
+} else if (m === 0) {
+  /* todo*/
+} else {
+  let m1 = m;
+  let n1 = n;
+  while (m1 !== n1) {
+    if (m1 > n1) {
+      m1 -= n1;
+    } else {
+      n1 -= m1;
+    }
+  }
+  m = m / m1;
+  n = n / n1;
+
+  console.log(m1, n1);
+  alert(m + "/" + n);
+}
